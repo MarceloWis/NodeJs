e34ad6406f3956a109279849090b968bab6d64ea
Marcelo Wischniowski <marcelowis@Marcelos-MacBook-Pro.local>
2020-01-02 16:54:31 -0300

    Adiciona e configura o sequelize e o mocha

--- 08-multi-db-postgres/db/strategies/postgres.js
+++ 08-multi-db-postgres/db/strategies/postgres.js
@@ -1,4 +1,58 @@
-class Postgres {
+const Sequelize = require('sequelize')
+const ICrud = require('./interfaces/interfaceCrud')
+
+class Postgres extends ICrud {
+    constructor() {
+        super()
+        this._driver = null;
+        this._herois = null;
+        this._connect()
+    }
+
+     _connect() {
+        this._driver = new Sequelize(
+            'heroes',
+            'marcelowis',
+            'minhasenhasecreta',
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorsAliases: false,
+            }
+        )
+    }
+     async isConnected() {
+        try {
+           await this._driver.authenticate()
+            return true
+        } catch (error) {
+            console.log("Error Postgres conection")
+        }
+    }
+    async _defineModel() {
+        this._herois = driver.define('heroes', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nome: {
+                type: Sequelize.STRING,
+                required: true,
+            },
+            poder: {
+                type: Sequelize.STRING,
+                required: true,
+            }
+        }, { 
+            tableName: 'TB_HEROIS',
+            freezeTableName: false,
+            timestamps: false
+         })
+         await Herois.sync()
+    }
     create(item) {
         return console.log("Item adicionado ao Postgres")
      }
---
+++ 08-multi-db-postgres/postgresExample.js
@@ -0,0 +1,44 @@
+// npm install sequelize pg-hstore pg --save
+
+const Sequelize = require('sequelize')
+const driver = new Sequelize(
+    'heroes',
+    'marcelowis',
+    'minhasenhasecreta',
+    {
+        host: 'localhost',
+        dialect: 'postgres',
+        quoteIdentifiers: false,
+        operatorsAliases: false,
+    }
+)
+
+async function main() {
+    const Herois = driver.define('herois', {
+        id: {
+            type: Sequelize.INTEGER,
+            required: true,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: Sequelize.STRING,
+            required: true,
+        },
+        poder: {
+            type: Sequelize.STRING,
+            required: true,
+        }
+    }, { 
+        tableName: 'TB_HEROIS',
+        freezeTableName: false,
+        timestamps: false
+     })
+     await Herois.sync()
+     await Herois.create({nome: 'Lanterna Verde', poder: 'Anel'})
+     const result = await Herois.findAll({ raw: true })
+    
+     console.log(result)
+}
+
+main()
